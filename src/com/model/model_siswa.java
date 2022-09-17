e47631cbd019752da7353de7aef293fa96e34cb2
Arissteiawan06 <arissetiawan261ss@gmail.com>
2022-09-17 10:00:21 +0700

    add ubah

--- src/com/model/model_siswa.java
+++ src/com/model/model_siswa.java
@@ -71,44 +71,44 @@ public class model_siswa implements control_siswa{
 
     @Override
     public void hapus(tampilan siswa) throws SQLException {
-        try {
-            Connection con = koneksi.getcon();
-            String sql = "DELETE FROM siswa WHERE NIS =?";
-            PreparedStatement prepare = con.prepareStatement(sql);
-            prepare.setString(1, siswa.txtNIS.getText());
-            prepare.executeUpdate();
-            JOptionPane.showMessageDialog(null, "Data berhasil dihapus.");
-            prepare.close();
-            Baru(siswa);
-        } catch (Exception e) {
-            System.out.println (e);
-        } finally {
-            Tampil(siswa);
-            siswa.setLebarKolom();
-            Baru(siswa);
-        }
+//        try {
+//            Connection con = koneksi.getcon();
+//            String sql = "DELETE FROM siswa WHERE NIS =?";
+//            PreparedStatement prepare = con.prepareStatement(sql);
+//            prepare.setString(1, siswa.txtNIS.getText());
+//            prepare.executeUpdate();
+//            JOptionPane.showMessageDialog(null, "Data berhasil dihapus.");
+//            prepare.close();
+//            Baru(siswa);
+//        } catch (Exception e) {
+//            System.out.println (e);
+//        } finally {
+//            Tampil(siswa);
+//            siswa.setLebarKolom();
+//            Baru(siswa);
+//        }
     }
 
     @Override
     public void Tampil(tampilan siswa) throws SQLException {
-        siswa.tblmodel.getDataVector() .removeAllElements();
-        siswa.tblmodel.fireTableDataChanged();
-        try{
-          Connection con = koneksi.getcon();
-          Statement stt = con.createStatement();
-          String sql = "SELECT * FROM siswa order BY NIS ASC";
-          ResultSet res = stt.executeQuery(sql);
-          while (res.next()){
-          Object[] ob = new Object[8];
-          ob[0] = res.getString(1);
-          ob[1] = res.getString(2);
-          ob[2] = res.getString(3);
-          ob[3] = res.getString(4);
-          siswa.tblmodel.addRow(ob);
-          }
-        } catch (SQLException e){
-            System.out.println(e);
-            }
+//        siswa.tblmodel.getDataVector() .removeAllElements();
+//        siswa.tblmodel.fireTableDataChanged();
+//        try{
+//          Connection con = koneksi.getcon();
+//          Statement stt = con.createStatement();
+//          String sql = "SELECT * FROM siswa order BY NIS ASC";
+//          ResultSet res = stt.executeQuery(sql);
+//          while (res.next()){
+//          Object[] ob = new Object[8];
+//          ob[0] = res.getString(1);
+//          ob[1] = res.getString(2);
+//          ob[2] = res.getString(3);
+//          ob[3] = res.getString(4);
+//          siswa.tblmodel.addRow(ob);
+//          }
+//        } catch (SQLException e){
+//            System.out.println(e);
+//            }
     }       
 
     @Override
@@ -121,21 +121,21 @@ public class model_siswa implements control_siswa{
 
     @Override
     public void KLikTabel(tampilan siswa) throws SQLException {
-        try {
-            int pilih = siswa.tabel.getSelectedRow();
-            if (pilih == -1){
-                return;
-        }
-        siswa.txtNIS.setText(siswa.tblmodel.getValueAt(pilih, 0).toString());
-        siswa.txtNama.setText(siswa.tblmodel.getValueAt(pilih, 1).toString());
-        siswa.cbJurusan.setSelectedItem(siswa.tblmodel.getValueAt(pilih, 3).toString());
-        jk = String.valueOf(siswa.tblmodel.getValueAt(pilih, 2));
-    } catch (Exception e) {
-    }
-    if (siswa.rbLaki.getText().equals(jk)) {
-        siswa.rbLaki.setSelected(true);
-    } else {
-        siswa.rbPerempuan.setSelected(true);
-    }
+//        try {
+//            int pilih = siswa.tabel.getSelectedRow();
+//            if (pilih == -1){
+//                return;
+//        }
+//        siswa.txtNIS.setText(siswa.tblmodel.getValueAt(pilih, 0).toString());
+//        siswa.txtNama.setText(siswa.tblmodel.getValueAt(pilih, 1).toString());
+//        siswa.cbJurusan.setSelectedItem(siswa.tblmodel.getValueAt(pilih, 3).toString());
+//        jk = String.valueOf(siswa.tblmodel.getValueAt(pilih, 2));
+//    } catch (Exception e) {
+//    }
+//    if (siswa.rbLaki.getText().equals(jk)) {
+//        siswa.rbLaki.setSelected(true);
+//    } else {
+//        siswa.rbPerempuan.setSelected(true);
+//    }
   }
 }
